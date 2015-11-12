a8c60547fac15eabe045f2845c199f4552f56983
Abeer <aalsumait@kacst.edu.sa>
2015-11-12 16:12:41 +0300

    Abeer adding Activities skelton and layouts

---
+++ app/src/main/java/com/example/salabdullatif/im/Homepage.java
@@ -0,0 +1,14 @@
+package com.example.salabdullatif.im;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Homepage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+
+}
---
+++ app/src/main/java/com/example/salabdullatif/im/Profile.java
@@ -0,0 +1,13 @@
+package com.example.salabdullatif.im;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Profile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
